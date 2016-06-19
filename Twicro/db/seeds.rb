4771cc9e237aaf33b828b7d5bb09372f0eb02e95
Kazuma Maekawa <yana.sd.iml@gmail.com>
2016-06-20 00:35:01 +0900

    バグフィックス

--- Twicro/db/seeds.rb
+++ Twicro/db/seeds.rb
@@ -61,3 +61,42 @@ Relation.create(temp_id:2, no1:4, index1:0, no2:5, index2:0)
 Relation.create(temp_id:2, no1:6, index1:2, no2:7, index2:1)
 #
 # =========================================
+
+# =========================================
+#     1 6   2 5
+#   0| | | | | |
+#    | |*|*| |*|
+#   3| | |*| | |4
+#    |*| |*|*| |
+#   7|*| | | | |
+#s
+Temp.create(height:5, width:5)
+
+Black.create(temp_id:3, column:0, row:3)
+Black.create(temp_id:3, column:0, row:4)
+Black.create(temp_id:3, column:1, row:1)
+Black.create(temp_id:3, column:2, row:1)
+Black.create(temp_id:3, column:2, row:2)
+Black.create(temp_id:3, column:2, row:3)
+Black.create(temp_id:3, column:3, row:3)
+Black.create(temp_id:3, column:4, row:1)
+
+White.create(temp_id:3, no:0, row:0, column:0, length:5, horizonal:true)
+White.create(temp_id:3, no:1, row:0, column:0, length:3, horizonal:false)
+White.create(temp_id:3, no:2, row:0, column:3, length:3, horizonal:false)
+White.create(temp_id:3, no:3, row:2, column:0, length:2, horizonal:true)
+White.create(temp_id:3, no:4, row:2, column:3, length:2, horizonal:true)
+White.create(temp_id:3, no:5, row:2, column:4, length:3, horizonal:false)
+White.create(temp_id:3, no:6, row:2, column:1, length:3, horizonal:false)
+White.create(temp_id:3, no:7, row:4, column:1, length:4, horizonal:true)
+
+Relation.create(temp_id:3, no1:0, index1:0, no2:1, index2:0)
+Relation.create(temp_id:3, no1:0, index1:0, no2:2, index2:0)
+Relation.create(temp_id:3, no1:1, index1:2, no2:3, index2:0)
+Relation.create(temp_id:3, no1:2, index1:2, no2:4, index2:0)
+Relation.create(temp_id:3, no1:4, index1:1, no2:5, index2:0)
+Relation.create(temp_id:3, no1:3, index1:1, no2:6, index2:0)
+Relation.create(temp_id:3, no1:6, index1:2, no2:7, index2:0)
+Relation.create(temp_id:3, no1:5, index1:1, no2:7, index2:3)
+#
+# =========================================
